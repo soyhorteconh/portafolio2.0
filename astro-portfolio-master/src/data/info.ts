7aa01c57ec6bfa6736e05a0caa823a2e2d31c841
Hortencia Alejandra Ramírez Vázquez <114839269+soyhorteconh@users.noreply.github.com>
2023-12-04 23:17:32 +0000

    update 2

--- astro-portfolio-master/src/data/info.ts
+++ astro-portfolio-master/src/data/info.ts
@@ -1,16 +1,9 @@
 export const info = {
   baseUrl: "https://astro-portfolio-uzair.vercel.app",
-  name: "Horte Ramírez",
+  name: "Horte Ramírez!",
   jobDescription: "Web Developer",
-  about: `I'm a software engineer based in Karachi, Pakistan.
-   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
-   I have been working for 3 year, with a demonstrated history of working in the Computer Software industry. Skilled in Front-end Development.
-   I have 3 years of experience in HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, StyledComponent, React JS and Next JS. Hence,
-   I have successfully completed several web projects from scratch. 
-   Yet, I learn continuously and never stop exploring new technologies.
-   So, I have also gained knowledge of Node JS, Express JS, Mongo DB, Python,
-   firebase and React Native with time.
-  `,
+  about: `I’m a Robotics engineering student @Tecnológico de Monterrey.`,
+  paragraph: 'My academic path has been fueled by an insatiable curiosity and a constant eagerness to learn about various robotics-related topics.',
 
   experience: [
     {
@@ -78,11 +71,9 @@ export const info = {
   ],
 
   socialMedia: {
-    facebook: "https://www.facebook.com/profile.php?id=100022899849583",
-    twitter: "https://twitter.com/Uzzii_21",
-    github: "https://github.com/uzzii-21",
-    email: "mailto:uzairahmed@74372gmail.com",
-    linkedin: "https://www.linkedin.com/in/uzzii21/",
+    github: "https://github.com/soyhorteconh",
+    email: "mailto:ha.ramirezv@outlook.com",
+    linkedin: "https://www.linkedin.com/in/hortencia-alejandra-ram%C3%ADrez-v%C3%A1zquez-7739ab220/",
   },
 
   projects: [
