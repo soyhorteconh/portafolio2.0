e7452796e8a3aaf50a4ced19617a3069aba5a711
Hortencia Alejandra Ramírez Vázquez <114839269+soyhorteconh@users.noreply.github.com>
2023-12-04 22:04:26 +0000

    update

--- astro-portfolio-master/src/data/info.ts
+++ astro-portfolio-master/src/data/info.ts
@@ -1,6 +1,6 @@
 export const info = {
   baseUrl: "https://astro-portfolio-uzair.vercel.app",
-  name: "Uzair Ahmed",
+  name: "Horte Ramírez",
   jobDescription: "Web Developer",
   about: `I'm a software engineer based in Karachi, Pakistan.
    I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
